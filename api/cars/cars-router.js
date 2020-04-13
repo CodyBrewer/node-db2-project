8b06509bf680297ce615f9789630535c6af57afa
CodyBrewer <codybrewerdev@gmail.com>
2020-04-13 13:12:02 -0600

    create basic routes for post, put, & delete

--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -28,4 +28,10 @@ router.get('/:id', async (req, res) => {
     }
 })
 
+router.post('/:id', async (req, res) => {});
+
+router.put('/:id', async (req, res) => {});
+
+router.delete('/:id', async (req, res) => {});
+
 module.exports = router;
